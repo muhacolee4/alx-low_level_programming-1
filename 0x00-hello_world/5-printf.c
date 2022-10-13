056722533867771cda69f624ae798e55247cf2e2
Isaac Ayegba <zicthingz@example.com>
2022-10-13 08:18:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - with the proper grammer
+ * but the outcome is a piece of art
+ * Return: zer(0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
