62583e19ca5725cd7aeeda1ab6b367cc88ec5830
Isaac Ayegba <zicthingz@example.com>
2022-10-13 07:45:23 -0700

    put 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Print
- *
- * Return :Always zero (0)
+ * main - Print jhgj
+ * Return: Always zero (0)
  */
 
 
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-
-	}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
