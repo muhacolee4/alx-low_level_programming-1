e02551fb96f2e43869234dc10472e51b5d386ac4
Isaac Ayegba <zicthingz@example.com>
2022-11-25 05:19:54 -0800

    git

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nodelen;
+	size_t s = 0;
 
-	nodelen = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0], (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		nodelen++;
+		s++;
 	}
-	return (nodelen);
+	return (s);
 }
